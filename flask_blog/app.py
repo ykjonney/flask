daf72a4a031157fa4386b1e73968185a3bac93af
jonney <jonney.hu@blockheaders.com>
2018-09-07 13:25:36 +0800

    add redis server

--- flask_blog/app.py
+++ flask_blog/app.py
@@ -1,8 +1,13 @@
+import redis
 from flask import Flask
+from flask_httpauth import HTTPTokenAuth
+
 from extensions import ext
 app = Flask(__name__)
 
 ext(app)
+auth=HTTPTokenAuth('flask')
+redis_client=redis.StrictRedis(host='localhost',port=6379,db=0)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
---
+++ flask_blog/utils/redis_server.py
@@ -0,0 +1,27 @@
+from flask import current_app
+
+from app import redis_client
+
+from itsdangerous import TimedJSONWebSignatureSerializer as Serialize
+
+def create_serialize():
+    return Serialize(current_app.config['SECRET_KEY'],expires_in=7200)
+# auth
+def redis_generate_token(user_id):
+    serialize=create_serialize()
+    token=serialize.dumps({'id':user_id}).decode('utf-8')
+    redis_client.hset(str(current_app.config['REDIS_TOKEN_KEY']),str(token),int(user_id))
+    return token
+def get_redis_token_info(token):
+    user_id=redis_client.hget(str(current_app.config["REDIS_TOKEN-KEY"]),str(token))
+    return user_id.decode('utf-8') if user_id else None
+def redis_del(token):
+    redis_client.hdel(str(current_app.config['REDIS-TOKEN_KEY']),str(token))
+
+# sms
+def redis_generate_sms_token(user_id):
+    pass
+def get_redis_sms_info(token):
+    pass
+def sms_redis_del():
+    pass
